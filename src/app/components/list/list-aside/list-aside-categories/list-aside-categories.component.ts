526dfb67d10235fcddabc403a05920fba6c37f87
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-02-23 21:12:56 +0100

    Update list-aside-categories component

--- src/app/components/list/list-aside/list-aside-categories/list-aside-categories.component.ts
+++ src/app/components/list/list-aside/list-aside-categories/list-aside-categories.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { CategoryEntity } from '../../../../entities/category-entity';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'list-aside-categories',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './list-aside-categories.component.html',
   styleUrl: './list-aside-categories.component.css'
 })
 export class ListAsideCategoriesComponent {
+
+  categories: CategoryEntity[] = [
+    {
+      id: 2,
+      name: 'Category 2',
+      articles: []
+    },
+    {
+      id: 3,
+      name: 'Category 3',
+      articles: []
+    }
+  ];
+
+  selectedCategory: number = 1;
 
 }
