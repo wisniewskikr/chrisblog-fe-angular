83a42cc520e13cfc72998cddcb9cce69779844b6
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-02-23 08:59:38 +0100

    Init TemplateEnum

---
+++ src/app/enums/TemplateEnum.ts
@@ -0,0 +1,10 @@
+enum TemplateEnum {
+
+    NONE = 'null',
+    CONTENT = 'content',
+    LINK_ALBUM = 'link-album',
+    LINK_DOCUMENT = 'link-document',
+    LINK_GITHUB = 'link-github',
+    LINK_YOUTUME = 'link-youtube'
+
+}
