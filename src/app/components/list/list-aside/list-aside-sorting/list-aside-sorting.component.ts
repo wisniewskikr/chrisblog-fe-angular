f81bcf9d9ec4190346aa4526e433064a3b43dc73
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-02-23 19:56:47 +0100

    Init list-aside-sorting

--- src/app/components/list/list-aside/list-aside-sorting/list-aside-sorting.component.ts
+++ src/app/components/list/list-aside/list-aside-sorting/list-aside-sorting.component.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { SortingEnum } from '../../../../enums/sorting-enum';
 
 @Component({
   selector: 'list-aside-sorting',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './list-aside-sorting.component.html',
   styleUrl: './list-aside-sorting.component.css'
 })
 export class ListAsideSortingComponent {
+
+  sorting: SortingEnum[] = [SortingEnum.AUTHOR_DECREASING, SortingEnum.AUTHOR_INCREASING, SortingEnum.DATE_DECREASING, 
+                            SortingEnum.DATE_INCREASING, SortingEnum.TITLE_DECREASING, SortingEnum.TITLE_INCREASING];
+
 
 }
