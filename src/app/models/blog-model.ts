6d4dec1fb9550999f55b9333360f0d2fe13746b2
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-02-23 17:24:30 +0100

    Init blog-model

---
+++ src/app/models/blog-model.ts
@@ -0,0 +1,33 @@
+import { PaginationModel } from "./pagination-model";
+
+export class BlogModel extends PaginationModel {
+
+    selectedCategory: string = "home";
+    selectedTag: number|null = null;
+    searchText: string|null = null;
+
+    public getSelectedCategory(): string {
+        return this.selectedCategory;
+    }
+
+    public setSelectedCategory(selectedCategory: string): void {
+        this.selectedCategory = selectedCategory;
+    }
+
+    public getSelectedTag(): number|null {
+        return this.selectedTag;
+    }
+
+    public setSelectedTag(selectedTag: number|null): void {
+        this.selectedTag = selectedTag;
+    }
+
+    public getSearchText(): string|null {
+        return this.searchText;
+    }
+
+    public setSearchText(searchText: string|null): void {
+        this.searchText = searchText;
+    }
+
+}
