47d48b74421644a8efe0d162c59037731286dd11
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-02-23 18:48:50 +0100

    Implement list-main-pagination

--- src/app/components/list-main-pagination/list-main-pagination.component.ts
+++ src/app/components/list-main-pagination/list-main-pagination.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PaginationModel } from '../../models/pagination-model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'list-main-pagination',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './list-main-pagination.component.html',
   styleUrl: './list-main-pagination.component.css'
 })
-export class ListMainPaginationComponent {
+export class ListMainPaginationComponent implements OnInit{  
+
+  pagination: PaginationModel|null = null;
+  
+  ngOnInit(): void {
+    this.pagination = new PaginationModel();
+    this.pagination.setCurrentPage(1);
+    this.pagination.setPages([1,2,3]);
+  }
 
 }
