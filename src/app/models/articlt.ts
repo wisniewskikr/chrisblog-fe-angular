fbf4319ba5b414107d6f9f7fb8574699d2c13597
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-02-23 08:48:58 +0100

    Init Article model

---
+++ src/app/models/articlt.ts
@@ -0,0 +1,17 @@
+export interface Article {
+
+    id: number;
+    pageDescription: string;
+    title: string;
+    description: string;
+    // TODO: check it
+    date: string;
+    author: string;
+    url: string;
+    content: string;
+    // TODO
+    // TemplateEnum 
+    // CategoryEntity category
+    // Set<TagEntity> tag
+
+}
