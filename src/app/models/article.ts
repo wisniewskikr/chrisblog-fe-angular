11e5fb1d4fdeabbff2f8b49fd2b9526e648b164c
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-02-23 08:55:20 +0100

    Update Article interface

--- src/app/models/article.ts
+++ src/app/models/article.ts
@@ -1,4 +1,5 @@
 import { Category } from "./category";
+import { Tag } from "./tag";
 
 export interface Article {
 
@@ -12,8 +13,8 @@ export interface Article {
     url: string;
     content: string;
     category: Category;
+    tags: Tag[];
     // TODO
     // TemplateEnum 
-    // Set<TagEntity> tag
 
 }
