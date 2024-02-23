b5931f8fb8bc8bea74807da8240c0d3e7086093a
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-02-23 20:21:30 +0100

    Implement list-aside-tag component

--- src/app/components/list/list-aside/list-aside-tag/list-aside-tag.component.ts
+++ src/app/components/list/list-aside/list-aside-tag/list-aside-tag.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { TagEntity } from '../../../../entities/tag-entity';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'list-aside-tag',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './list-aside-tag.component.html',
   styleUrl: './list-aside-tag.component.css'
 })
 export class ListAsideTagComponent {
+
+  tags: TagEntity[] = [
+    {
+      id: 1,
+      name: 'Tag 1',
+      articles: []
+    }
+  ];
+
+  selectedTag: number = 1;
+  selectedCategory: string = 'IT';  
 
 }
