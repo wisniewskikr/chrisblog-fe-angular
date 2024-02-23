c49d1333467c23360309ade57c7d78f6a323b53f
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-02-23 20:07:35 +0100

    Implement list-aside-search

--- src/app/components/list/list-aside/list-aside-search/list-aside-search.component.ts
+++ src/app/components/list/list-aside/list-aside-search/list-aside-search.component.ts
@@ -1,12 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'list-aside-search',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './list-aside-search.component.html',
   styleUrl: './list-aside-search.component.css'
 })
 export class ListAsideSearchComponent {
+
+  searchText: string = "";
 
 }
