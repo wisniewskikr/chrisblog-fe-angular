e0aa048508568f85900c807cb947b0327c99feb7
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-02-24 16:54:21 +0100

    Implement params in aside sorting

--- src/app/components/list/list-aside/list-aside-sorting/list-aside-sorting.component.ts
+++ src/app/components/list/list-aside/list-aside-sorting/list-aside-sorting.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { SortingEnum } from '../../../../enums/sorting-enum';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'list-aside-sorting',
@@ -13,6 +14,33 @@ export class ListAsideSortingComponent {
 
   sorting: SortingEnum[] = [SortingEnum.AUTHOR_DECREASING, SortingEnum.AUTHOR_INCREASING, SortingEnum.DATE_DECREASING, 
                             SortingEnum.DATE_INCREASING, SortingEnum.TITLE_DECREASING, SortingEnum.TITLE_INCREASING];
+
+  categoryId: string|null = null;
+  selectedSoring: string|null = null;
+  page: string|null = null;
+  searchText: string|null = null;
+  tagId: string|null = null;
+  
+  constructor(private activatedRoute: ActivatedRoute, private router: Router) {
+
+    this.activatedRoute.params.subscribe(params => {
+      this.categoryId = params['categoryId'];
+      this.selectedSoring = params['sorting'];
+      this.page = params['page'];
+    });
+
+    this.activatedRoute.queryParams.subscribe(params => {
+      this.searchText = params['searchtext'];
+      this.tagId = params['tagid'];
+    });
+
+  }
+
+  onChange(event: any) {
+    this.selectedSoring = event.target.value;
+    const path = `category/${this.categoryId}/sorting/${this.selectedSoring}/page/${this.page}`;
+    this.router.navigate([path], { queryParams: { searchtext: this.searchText, tagid: this.tagId } });
+  }
 
 
 }
