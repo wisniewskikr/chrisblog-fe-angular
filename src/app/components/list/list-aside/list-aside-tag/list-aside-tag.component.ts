462e738af0e9d714e8fc056eb574f0e0e5c78667
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-02-24 17:19:12 +0100

    Init tags

--- src/app/components/list/list-aside/list-aside-tag/list-aside-tag.component.ts
+++ src/app/components/list/list-aside/list-aside-tag/list-aside-tag.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { TagEntity } from '../../../../entities/tag-entity';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'list-aside-tag',
@@ -16,10 +17,33 @@ export class ListAsideTagComponent {
       id: 1,
       name: 'Tag 1',
       articles: []
+    },
+    {
+      id: 2,
+      name: 'Tag 2',
+      articles: []
     }
   ];
 
-  selectedTag: number = 1;
-  selectedCategory: string = 'IT';  
+  categoryId: number|null = null;
+  soring: string|null = null;
+  page: string|null = null;
+  searchText: string|null = null;
+  tagId: number|null = null;
+  
+  constructor(private activatedRoute: ActivatedRoute, private router: Router) {
+
+    this.activatedRoute.params.subscribe(params => {
+      this.categoryId = Number(params['categoryId']);
+      this.soring = params['sorting'];
+      this.page = params['page'];
+    });
+
+    this.activatedRoute.queryParams.subscribe(params => {
+      this.searchText = params['searchtext'];
+      this.tagId = (params['tagid'] != undefined) ? Number(params['tagid']) : null;
+    });
+
+  }    
 
 }
