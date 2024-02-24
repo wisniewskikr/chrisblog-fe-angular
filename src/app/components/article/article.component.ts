97883b17f3e8f89686ccf1dc2e2f401c95f0081f
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-02-24 10:32:27 +0100

    Implement ArticleContent component

--- src/app/components/article/article.component.ts
+++ src/app/components/article/article.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { ArticleContentComponent } from './article-content/article-content.component';
+import { ArticleEntity } from '../../entities/article-entity';
+import { TemplateEnum } from '../../enums/template-enum';
 
 @Component({
   selector: 'article',
@@ -9,5 +11,23 @@ import { ArticleContentComponent } from './article-content/article-content.compo
   styleUrl: './article.component.css'
 })
 export class ArticleComponent {
+
+  article: ArticleEntity = {
+    id: 1,
+    author: 'Author 1',
+    category: {
+      id: 1,
+      name: 'Category 1',
+      articles: []
+    },
+    content: 'Content 1',
+    date: 'Date 1',
+    description: 'Description 1',
+    pageDescription: 'Page Description 1',
+    tags: [],
+    template: TemplateEnum.CONTENT,
+    title: 'Title 1',
+    url: 'Url 1'
+  };
 
 }
