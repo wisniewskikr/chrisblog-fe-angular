23a5288299a75d2ffacae740bc787a242a94953d
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-02-24 17:50:26 +0100

    Init implementation of searchText

--- src/app/components/list/list-aside/list-aside-search/list-aside-search.component.ts
+++ src/app/components/list/list-aside/list-aside-search/list-aside-search.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'list-aside-search',
@@ -10,6 +11,39 @@ import { Component } from '@angular/core';
 })
 export class ListAsideSearchComponent {
 
-  searchText: string = "";
+  categoryId: number|null = null;
+  soring: string|null = null;
+  page: string|null = null;
+  searchText: string|null = null;
+  tagId: number|null = null;
+  
+  constructor(private activatedRoute: ActivatedRoute, private router: Router) {
+
+    this.activatedRoute.params.subscribe(params => {
+      this.categoryId = Number(params['categoryId']);
+      this.soring = params['sorting'];
+      this.page = params['page'];
+    });
+
+    this.activatedRoute.queryParams.subscribe(params => {
+      this.searchText = params['searchtext'] ?? null;
+      this.tagId = (params['tagid'] != undefined) ? Number(params['tagid']) : null;
+    });
+
+  }
+
+  onClickSearch(text: string) {
+    this.searchText = text;
+    this.tagId = null;
+    const path = `category/${this.categoryId}/sorting/${this.soring}/page/${this.page}`;
+    this.router.navigate([path], { queryParams: { searchtext: this.searchText, tagid: this.tagId } });
+  }
+
+  onClickClear() {
+    this.searchText = null;
+    this.tagId = null;
+    const path = `category/${this.categoryId}/sorting/${this.soring}/page/${this.page}`;
+    this.router.navigate([path], { queryParams: { searchtext: this.searchText, tagid: this.tagId } });
+  }
 
 }
