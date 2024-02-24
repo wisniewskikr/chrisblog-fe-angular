7a7592f217a16f2728581e77196e68fd8355ab3c
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-02-24 13:49:05 +0100

    Implement params for sorting

--- src/app/components/list/list-main/list-main-sorting/list-main-sorting.component.ts
+++ src/app/components/list/list-main/list-main-sorting/list-main-sorting.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SortingEnum } from '../../../../enums/sorting-enum';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'list-main-sorting',
@@ -13,5 +14,20 @@ export class ListMainSortingComponent {
 
   sorting: SortingEnum[] = [SortingEnum.AUTHOR_DECREASING, SortingEnum.AUTHOR_INCREASING, SortingEnum.DATE_DECREASING, 
                             SortingEnum.DATE_INCREASING, SortingEnum.TITLE_DECREASING, SortingEnum.TITLE_INCREASING];
+  
+  constructor(private activatedRoute: ActivatedRoute) {
+
+    this.activatedRoute.params.subscribe(params => {
+      console.log("ListMainSortingComponent - category: " + params['category']);
+      console.log("ListMainSortingComponent - sorting: " + params['sorting']);
+      console.log("ListMainSortingComponent - page: " + params['page']);
+    });
+
+    this.activatedRoute.queryParams.subscribe(params => {
+      console.log("ListMainSortingComponent - searchtext: " + params['searchtext']);
+      console.log("ListMainSortingComponent - tags: " + params['tags']);
+    });
+
+  }
 
 }
