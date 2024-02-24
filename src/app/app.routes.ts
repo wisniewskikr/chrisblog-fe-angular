9537a5a172af0a6c080d22ac83d9b5b490e0fc0f
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-02-24 13:03:46 +0100

    Update Route collection

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,8 @@ import { AboutComponent } from './components/about/about.component';
 import { ArticleComponent } from './components/article/article.component';
 
 export const routes: Routes = [
-    { path: '', component: ListComponent },
+    { path: '', redirectTo: 'category/home/sorting/date_decreasing/page/1', pathMatch: 'full' },
+    { path: 'category/:category/sorting/:sorting/page/:page', component: ListComponent },
     { path: 'about', component: AboutComponent },
     { path: 'article/:id', component: ArticleComponent }
 ];
