13214cdbe3a041f30657b3f683c83f86ac69edd8
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-02-24 12:32:26 +0100

    Implement Article type link-document

--- src/app/components/article/article.component.ts
+++ src/app/components/article/article.component.ts
@@ -25,7 +25,7 @@ export class ArticleComponent {
     description: 'Description 1',
     pageDescription: 'Page Description 1',
     tags: [],
-    template: TemplateEnum.LINK_ALBUM,
+    template: TemplateEnum.LINK_DOCUMENT,
     title: 'Title 1',
     url: 'Url 1'
   };
