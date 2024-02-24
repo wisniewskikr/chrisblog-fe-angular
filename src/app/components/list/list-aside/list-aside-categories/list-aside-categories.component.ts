8ab3098e31e63c9df5317da9571184bee1cd5c3d
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-02-24 17:10:58 +0100

    Implement category list

--- src/app/components/list/list-aside/list-aside-categories/list-aside-categories.component.ts
+++ src/app/components/list/list-aside/list-aside-categories/list-aside-categories.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CategoryEntity } from '../../../../entities/category-entity';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'list-aside-categories',
@@ -14,17 +14,42 @@ export class ListAsideCategoriesComponent {
 
   categories: CategoryEntity[] = [
     {
-      id: 2,
-      name: 'Category 2',
+      id: 1,
+      name: 'Category 1',
       articles: []
     },
     {
-      id: 3,
-      name: 'Category 3',
+      id: 2,
+      name: 'Category 2',
       articles: []
     }
   ];
 
-  selectedCategory: number = 1;
+  selectedCategory: number|null = null;
+  soring: string|null = null;
+  page: string|null = null;
+  searchText: string|null = null;
+  tagId: string|null = null;
+  
+  constructor(private activatedRoute: ActivatedRoute, private router: Router) {
+
+    this.activatedRoute.params.subscribe(params => {
+      this.selectedCategory = Number(params['categoryId']);
+      this.soring = params['sorting'];
+      this.page = params['page'];
+    });
+
+    this.activatedRoute.queryParams.subscribe(params => {
+      this.searchText = params['searchtext'];
+      this.tagId = params['tagid'];
+    });
+
+  }
+
+  onClick(category: number) {
+    this.selectedCategory = category;
+    const path = `category/${this.selectedCategory}/sorting/${this.soring}/page/${this.page}`;
+    this.router.navigate([path], { queryParams: { searchtext: this.searchText, tagid: this.tagId } });
+  }
 
 }
