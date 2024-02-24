7d559b78940e7db9b60d4e4548b735092de06125
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-02-24 10:13:56 +0100

    Init ArticleContent component

---
+++ src/app/components/article/article-content/article-content.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'article-content',
+  standalone: true,
+  imports: [],
+  templateUrl: './article-content.component.html',
+  styleUrl: './article-content.component.css'
+})
+export class ArticleContentComponent {
+
+}
--- src/app/components/article/article.component.ts
+++ src/app/components/article/article.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ArticleContentComponent } from './article-content/article-content.component';
 
 @Component({
   selector: 'article',
   standalone: true,
-  imports: [],
+  imports: [ArticleContentComponent],
   templateUrl: './article.component.html',
   styleUrl: './article.component.css'
 })
