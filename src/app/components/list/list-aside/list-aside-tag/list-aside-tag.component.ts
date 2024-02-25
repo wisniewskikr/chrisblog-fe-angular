3cbd43a9753f6ce5b25610eb34f9675f79e9ecbc
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-02-25 17:47:36 +0100

    Add subscription to list-aside-tag component

--- src/app/components/list/list-aside/list-aside-tag/list-aside-tag.component.ts
+++ src/app/components/list/list-aside/list-aside-tag/list-aside-tag.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TagEntity } from '../../../../entities/tag-entity';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { TagService } from '../../../../services/tag.service';
+import { TagRequest } from '../../../../dtos/tag-request';
 
 @Component({
   selector: 'list-aside-tag',
@@ -10,33 +12,23 @@ import { ActivatedRoute, Router, RouterLink } from '@angular/router';
   templateUrl: './list-aside-tag.component.html',
   styleUrl: './list-aside-tag.component.css'
 })
-export class ListAsideTagComponent {
+export class ListAsideTagComponent implements OnInit {
 
-  tags: TagEntity[] = [
-    {
-      id: 1,
-      name: 'Tag 1',
-      articles: []
-    },
-    {
-      id: 2,
-      name: 'Tag 2',
-      articles: []
-    }
-  ];
-
+  tags: TagEntity[] = [];
   categoryId: number|null = null;
-  soring: string|null = null;
-  page: string|null = null;
+  sorting: string|null = null;
+  page: number|null = null;
   searchText: string|null = null;
   tagId: number|null = null;
   
-  constructor(private activatedRoute: ActivatedRoute, private router: Router) {
+  constructor(private activatedRoute: ActivatedRoute, private router: Router, private tagService: TagService) {}
 
+  ngOnInit(): void {
+    
     this.activatedRoute.params.subscribe(params => {
       this.categoryId = Number(params['categoryId']);
-      this.soring = params['sorting'];
-      this.page = params['page'];
+      this.sorting = params['sorting'];
+      this.page = Number(params['page']);
     });
 
     this.activatedRoute.queryParams.subscribe(params => {
@@ -44,12 +36,27 @@ export class ListAsideTagComponent {
       this.tagId = (params['tagid'] != undefined) ? Number(params['tagid']) : null;
     });
 
+    if (this.categoryId == null || this.page == null || this.sorting == null) {
+      throw new Error("Atributes 'categoryId', 'page' and 'sorting' are required.");
+    }
+    
+    this.handleTagApi(new TagRequest(this.categoryId, this.tagId, this.page, this.sorting, this.searchText));
+
+  }
+
+  handleTagApi(tagRequest: TagRequest): void {
+
+    let response = this.tagService.findTags(tagRequest);
+    response.subscribe((data)=>{      
+      this.tags = data.tags;
+    });
+
   }
   
   onClick(tag: number) {
     this.tagId = (this.tagId != tag) ? tag : null;
     this.searchText = null;
-    const path = `category/${this.categoryId}/sorting/${this.soring}/page/${this.page}`;
+    const path = `category/${this.categoryId}/sorting/${this.sorting}/page/${this.page}`;
     this.router.navigate([path], { queryParams: { searchtext: this.searchText, tagid: this.tagId } });
   }
 
