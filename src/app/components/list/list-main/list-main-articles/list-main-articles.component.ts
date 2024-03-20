dac93ba44063f5412d82d71360884e533898a3a3
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-03-20 17:57:23 +0100

    Add articles to ListMainComponent

--- src/app/components/list/list-main/list-main-articles/list-main-articles.component.ts
+++ src/app/components/list/list-main/list-main-articles/list-main-articles.component.ts
@@ -1,12 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ArticleEntity } from '../../../../entities/article-entity';
-import { ArticleService } from '../../../../services/article.service';
-import { ArticleRequest } from '../../../../dtos/article-request';
-import { PaginationService } from '../../../../services/pagination.service';
-import { PaginationDto } from '../../../../dtos/pagination-dto';
 import { ListMainArticlesArticleComponent } from './list-main-articles-article/list-main-articles-article.component';
-import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'list-main-articles',
@@ -15,71 +10,8 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './list-main-articles.component.html',
   styleUrl: './list-main-articles.component.css'
 })
-export class ListMainArticlesComponent implements OnInit {
+export class ListMainArticlesComponent {
 
-  articles: ArticleEntity[] = [];
-  categoryId: number|null = null;
-  tagId: number|null = null;
-  page: number|null = null;
-  sorting: string|null = null;
-  searchText: string|null = null;
-
-  constructor(private activatedRoute: ActivatedRoute, private articleService: ArticleService, 
-    private paginationService: PaginationService) {}
-
-  ngOnInit(): void {
-    
-    this.activatedRoute.params.subscribe(params => {
-
-      this.categoryId = Number(params['categoryId']);
-      this.sorting = params['sorting'];
-      this.page = Number(params['page']);
-
-      if (this.categoryId == null || this.page == null || this.sorting == null) {
-        throw new Error("Atributes 'categoryId', 'page' and 'sorting' are required.");
-      }
-
-      this.handleArtilceApi(new ArticleRequest(this.categoryId, this.tagId, this.page, this.sorting, this.searchText));
-            
-    });
-
-    this.activatedRoute.queryParams.subscribe(params => {
-
-      const searchTextParam = params['searchtext'];
-      const tagIdParam = params['tagid'];
-
-      if (searchTextParam == undefined && tagIdParam == undefined &&
-        this.searchText == null && this.tagId == null) {
-          return;
-      }
-
-      this.searchText = searchTextParam ?? null;
-      this.tagId = (tagIdParam != undefined) ? Number(tagIdParam) : null;
-
-      if (this.categoryId == null || this.page == null || this.sorting == null) {
-        throw new Error("Atributes 'categoryId', 'page' and 'sorting' are required.");
-      }
-
-      this.handleArtilceApi(new ArticleRequest(this.categoryId, this.tagId, this.page, this.sorting, this.searchText));
-      
-    });    
-
-  }
-
-  handleArtilceApi(articleRequest: ArticleRequest): void {
-
-    let response = this.articleService.findArticles(articleRequest);
-    response.subscribe((data)=>{      
-      
-      this.articles = data.articles;
-
-      if (this.page == null) {
-        throw new Error("Attribute 'page' is required.");
-      }
-      this.paginationService.updatePagination(new PaginationDto(this.page, data.pages, data.disablePrevious, data.disableNext));
-
-    });
-
-  }
+  @Input() articles: ArticleEntity[] = [];  
 
 }
--- src/app/components/list/list-main/list-main.component.ts
+++ src/app/components/list/list-main/list-main.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListMainSortingComponent } from './list-main-sorting/list-main-sorting.component';
 import { ListMainPaginationComponent } from './list-main-pagination/list-main-pagination.component';
 import { ListMainArticlesComponent } from './list-main-articles/list-main-articles.component';
 import { ArticleEntity } from '../../../entities/article-entity';
-import { TemplateEnum } from '../../../enums/template-enum';
+import { ActivatedRoute } from '@angular/router';
+import { ArticleService } from '../../../services/article.service';
+import { PaginationService } from '../../../services/pagination.service';
+import { ArticleRequest } from '../../../dtos/article-request';
+import { PaginationDto } from '../../../dtos/pagination-dto';
 
 @Component({
   selector: 'list-main',
@@ -13,55 +17,71 @@ import { TemplateEnum } from '../../../enums/template-enum';
   templateUrl: './list-main.component.html',
   styleUrl: './list-main.component.css'
 })
-export class ListMainComponent {
+export class ListMainComponent implements OnInit {
 
-  articles: ArticleEntity[] = [
-    {
-      id: 1, 
-      pageDescription: 'Page description 1',
-      author: 'Author 1',
-      content: 'Content 1',
-      date: 'Date 1',
-      description: 'Description 1',
-      template: TemplateEnum.CONTENT,
-      title: 'Title 1',
-      url: 'Url 1',
-      category: {
-        id: 1,
-        name: 'Category 1',
-        articles: []
-      },
-      tags: [
-        {
-          id: 1,
-          name: 'Tag 1',
-          articles: []
-        }
-      ]
-    },
-    // {
-    //   id: 2, 
-    //   pageDescription: 'Page description 2',
-    //   author: 'Author 2',
-    //   content: 'Content 2',
-    //   date: 'Date 2',
-    //   description: 'Description 2',
-    //   template: TemplateEnum.CONTENT,
-    //   title: 'Title 2',
-    //   url: 'Url 2',
-    //   category: {
-    //     id: 2,
-    //     name: 'Category 2',
-    //     articles: []
-    //   },
-    //   tags: [
-    //     {
-    //       id: 2,
-    //       name: 'Tag 2',
-    //       articles: []
-    //     }
-    //   ]
-    // }
-  ];
+  articles: ArticleEntity[] = [];
+  categoryId: number|null = null;
+  tagId: number|null = null;
+  page: number|null = null;
+  sorting: string|null = null;
+  searchText: string|null = null;
+
+  constructor(private activatedRoute: ActivatedRoute, private articleService: ArticleService, 
+    private paginationService: PaginationService) {}
+
+  ngOnInit(): void {
+    
+    this.activatedRoute.params.subscribe(params => {
+
+      this.categoryId = Number(params['categoryId']);
+      this.sorting = params['sorting'];
+      this.page = Number(params['page']);
+
+      if (this.categoryId == null || this.page == null || this.sorting == null) {
+        throw new Error("Atributes 'categoryId', 'page' and 'sorting' are required.");
+      }
+
+      this.handleArtilceApi(new ArticleRequest(this.categoryId, this.tagId, this.page, this.sorting, this.searchText));
+            
+    });
+
+    this.activatedRoute.queryParams.subscribe(params => {
+
+      const searchTextParam = params['searchtext'];
+      const tagIdParam = params['tagid'];
+
+      if (searchTextParam == undefined && tagIdParam == undefined &&
+        this.searchText == null && this.tagId == null) {
+          return;
+      }
+
+      this.searchText = searchTextParam ?? null;
+      this.tagId = (tagIdParam != undefined) ? Number(tagIdParam) : null;
+
+      if (this.categoryId == null || this.page == null || this.sorting == null) {
+        throw new Error("Atributes 'categoryId', 'page' and 'sorting' are required.");
+      }
+
+      this.handleArtilceApi(new ArticleRequest(this.categoryId, this.tagId, this.page, this.sorting, this.searchText));
+      
+    });    
+
+  }
+
+  handleArtilceApi(articleRequest: ArticleRequest): void {
+
+    let response = this.articleService.findArticles(articleRequest);
+    response.subscribe((data)=>{      
+      
+      this.articles = data.articles;
+
+      if (this.page == null) {
+        throw new Error("Attribute 'page' is required.");
+      }
+      this.paginationService.updatePagination(new PaginationDto(this.page, data.pages, data.disablePrevious, data.disableNext));
+
+    });
+
+  }
 
 }
